f3d8090738be0287f1172db86036a3503053161a
Vinci <fengkejob@163.com>
2022-07-21 19:02:36 +0800

    feat(behavioral): 添加访问者模式

--- src/main/java/com/vinci/behavioral/visitor/VisitorMain.java
+++ src/main/java/com/vinci/behavioral/visitor/VisitorMain.java
@@ -8,11 +8,11 @@ public class VisitorMain {
 
     public static void main(String[] args) {
         ObjectStructure collection = new ObjectStructure();
-        System.out.println("ConcreteVisitorA handle elements:");
+        System.out.println("ConcreteVisitorA handle elements: ");
         IVisitor visitorA = new ConcreteVisitorA();
         collection.accept(visitorA);
         System.out.println("--------------------------------");
-        System.out.println("ConcreteVisitorB handle elements:");
+        System.out.println("ConcreteVisitorB handle elements: ");
         IVisitor visitorB = new ConcreteVisitorB();
         collection.accept(visitorB);
     }
